06459a0329e1a92fd6a0604170c143fc7a5cac2e
lkmandy <shafack.likhene@gmail.com>
2020-09-10 01:55:50 +0100

    Add employer reviews feature

--- jobs/app.py
+++ jobs/app.py
@@ -1,5 +1,6 @@
-from flask import Flask, g
-from flask import render_template
+from flask import Flask, g, request
+from flask import render_template, redirect, url_for
+import datetime
 import sqlite3
 
 PATH = 'db/jobs.sqlite'
@@ -63,3 +64,18 @@ def employer(employer_id):
                           'employer.id = review.employer_id WHERE employer.id = ?', [employer_id])
     return render_template('employer.html', employer=employer, jobs=jobs, reviews=reviews)
 
+
+@app.route("/employer/<employer_id>/review")
+def review(employer_id, methods=('GET', 'POST')):
+    if request.method is 'POST':
+        review = request.form['review']
+        rating = request.form['rating']
+        title = request.form['title']
+        status = request.form['status']
+        date = datetime.datetime.now().strftime("%m/%d%Y")
+        execute_sql('INSERT INTO review (review, rating, title, date, status, employer_id) VALUES (?, ?, ?, ?, ?, ?)',
+                    (review, rating, title, date, status, employer_id), commit=True)
+
+        return redirect(url_for('employer', employer_id=employer_id))
+
+    return render_template("review.html", employer_id=employer_id)
