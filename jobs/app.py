b55e279c1d108ff61d117480cc7ab51240ba626b
lkmandy <shafack.likhene@gmail.com>
2020-09-22 13:23:42 +0100

    Add forward slash to format date properly

--- jobs/app.py
+++ jobs/app.py
@@ -1,3 +1,4 @@
+# import g (global namespace) to provide access to the database throughout the application
 from flask import Flask, g, request
 from flask import render_template, redirect, url_for
 import datetime
@@ -9,13 +10,17 @@ app = Flask(__name__)
 
 
 def open_connection():
+    # Global database attribute
     connection = getattr(g, '_connection', None)
     if connection is None:
+        # Global database connection
         connection = g._connection = sqlite3.connect(PATH)
+    # To make accessing data easier. All rows returned from the database will be named tuples.
     connection.row_factory = sqlite3.Row
     return connection
 
 
+# Query Database Function: used to query the database
 def execute_sql(sql, values=(), commit=False, single=False):
     connection = open_connection()
     cursor = connection.execute(sql, values)
@@ -28,6 +33,7 @@ def execute_sql(sql, values=(), commit=False, single=False):
     return results
 
 
+# Makes sure the database connection is closed when the app_context is destroyed
 @app.teardown_appcontext
 def close_connection(exception):
     connection = getattr(g, '_connection', None)
@@ -48,6 +54,7 @@ def job(job_id):
     job = execute_sql('SELECT job.id, job.title, job.description, job.salary, employer.id as employer_id, '
                       'employer.name as employer_name FROM job JOIN employer ON employer.id = job.employer_id WHERE '
                       'job.id = ?', [job_id], single=True)
+    # job=job : give the template access to the job data
     return render_template('job.html', job=job)
 
 
@@ -67,13 +74,12 @@ def employer(employer_id):
 
 @app.route("/employer/<employer_id>/review")
 def review(employer_id, methods=('GET', 'POST')):
-    if request.method == 'POST':
+    if request.method is 'POST':
         review = request.form['review']
         rating = request.form['rating']
         title = request.form['title']
         status = request.form['status']
-
-        date = datetime.datetime.now().strftime("%m/%d%Y")
+        date = datetime.datetime.now().strftime("%m/%d/%Y")
 
         execute_sql('INSERT INTO review (review, rating, title, date, status, employer_id) VALUES (?, ?, ?, ?, ?, ?)',
                     (review, rating, title, date, status, employer_id), commit=True)
